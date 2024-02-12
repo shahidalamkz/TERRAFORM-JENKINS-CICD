8bc26239ff2b3349848e3c3ce4bedd9b745a51f7
shahidalamkz <77041444+shahidalamkz@users.noreply.github.com>
2024-02-12 20:21:16 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "ap-south-1"
+  default     = "us-east-1"
 }
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
@@ -12,12 +12,12 @@ variable "instance_type" {
 }
 variable "ami_id" {
   description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0f5ee92e2d63afc18"
+  default     = "ami-0c7217cdde317cfec"
 }
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "shahidbucket001"
+  default     = "shahid-cloudbook"
 }
 
 variable "acl" {
